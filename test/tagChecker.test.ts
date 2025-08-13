d252903af1077fa0e1e665501a1b02df1f7727e7
Chris Stehlin <castehlin@gmail.com>
2025-08-13 22:33:00 +1200

    Initial commit

---
+++ test/tagChecker.test.ts
@@ -0,0 +1,7 @@
+describe('checkTags', () => {
+    describe('test cases that should pass, includes ones from example', () => {
+    });
+
+    describe('test cases that should fail, including ones from example', () => {
+    });
+});
